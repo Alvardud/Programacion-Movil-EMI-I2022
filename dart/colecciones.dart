4c699b64c3dc0f31b916b528e6646160b75a4db8
Alvardud <alvaro_dudutex@outlook.es>
2022-02-15 19:15:14 -0400

    Se agrego el archivo excepciones.dart

--- dart/colecciones.dart
+++ dart/colecciones.dart
@@ -1,3 +1,6 @@
 void main(){
   print('hola mundo');
+
+  // soy un comentario
+  print("Este print es para ver como funciona git en vs code");
 }
---
+++ dart/excepciones.dart
@@ -0,0 +1,3 @@
+void main(){
+  print("Soy una excepcion");
+}
