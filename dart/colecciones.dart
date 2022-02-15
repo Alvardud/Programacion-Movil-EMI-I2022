1912f9e7143910760ce9cae000375405320d5df4
Alvaro Martinez <alvaro_dudutex@outlook.es>
2022-02-15 19:40:59 -0400

    cambios realizados mediante live share

--- dart/colecciones.dart
+++ dart/colecciones.dart
@@ -1,6 +1,10 @@
 void main(){
-  print('hola mundo');
+  print('hola ');
 
-  // soy un comentario
+  // soy un comentario de parte de jorge
+
   print("Este print es para ver como funciona git en vs code");
+  print("HOLA !!!!!"); 
+  print("Las Clases Están Buenas");
+
 }
